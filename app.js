4a157427884a44bc5d0bfa5e40f6a2666aaf2a7a
DavidRZuluaga <git@davidrzuluaga.com>
2019-08-14 23:40:26 -0500

    hotfix heroku port 300

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const errorHandler = require('errorhandler');
 
 // const isProduction = process.env.NODE_ENV === 'production';
 // set up port
-const port = 5000;
+const port = 3000;
 // set up dependencies
 const app = express();
 app.use(bodyParser.json());
